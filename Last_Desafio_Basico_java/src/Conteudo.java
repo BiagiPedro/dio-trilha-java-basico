608bf8788b8cb8d5532b28490b55e202a7b87aef
Pedro Biagi <pedro.biagi@ufu.br>
2024-08-30 10:26:04 -0300

    commit teste do spring feito em maquina local

--- Last_Desafio_Basico_java/src/Conteudo.java
+++
@@ -1,9 +0,0 @@
-public abstract class Conteudo {
-    protected String titulo;
-    protected String descricao;
-    protected static final double XP_PADRAO = 10d;
-
-    public abstract double calcularXP();
-
-
-}
